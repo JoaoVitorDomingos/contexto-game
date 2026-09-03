5e88a57c9ac6f71b190312e894c88567eb68af56
JoaoVitorDomingos <domingosjoaovitor81@gmail.com>
2026-09-03 10:54:34 -0300

    chore(constantes): adiciona constantes

---
+++ shared/constants.py
@@ -0,0 +1,10 @@
+NOME_DO_JOGO = "PONTEXTO"
+
+NOME_INTEGRANTE_1 = "Guilherme Henrique de Sousa"
+NOME_INTEGRANTE_2 = "João Vitor C. Domingos"
+
+NOME_DISCIPLINA_1 = "Inteligência Artificial"
+NOME_DISCIPLINA_2 = "Redes de Computadores e Sistemas Distribuídos"
+
+NOME_CURSO = "Bacharelado em Ciência da Computação"
+NOME_UNIVERSIDADE = "Universidade Estadual do Paraná"
